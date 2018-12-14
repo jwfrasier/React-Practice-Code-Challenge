40aed11963c3ab7bfe77c662dcc540e1cd4a45a8
jwfrasier <frasier.joseph@gmail.com>
2018-12-14 09:15:21 -0600

    this aint working

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,46 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allOurSushi: [],
+      wallet: 100,
+      eaten: false,
+      displayIndex: 0
+    };
+  }
+
+  nextFourSushi = () => {
+    return this.state.allOurSushi.slice(
+      this.state.displayIndex,
+      this.state.displayIndex + 4
+    );
+  };
+
+  componentDidMount = () => {
+    fetch(API)
+      .then(response => response.json())
+      .then(sushi => this.setState({ allOurSushi: sushi }));
+  };
 
   render() {
+    // console.log(this.state.allOurSushi);
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          allFourSushi={this.state.allOurSushi.slice(0, 4)}
+          nextFourSushi={this.nextFourSushi}
+        />
+        <Table
+          allFourSushi={this.state.allOurSushi.slice(0, 4)}
+          wallet={this.state.wallet}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,24 @@
-import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import React, { Fragment } from "react";
+import MoreButton from "../components/MoreButton";
+import Sushi from "../components/Sushi";
 
-const SushiContainer = (props) => {
+const SushiContainer = props => {
+  const mapOverSushi = () => {
+    return props.nextFourSushi.map(sushi => {
+      return <Sushi sushi={sushi} key={sushi.id} />;
+    });
+  };
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {mapOverSushi()}
+        <MoreButton
+          moreSushi={props.allFourSushi}
+          nextFourSushi={props.nextFourSushi}
+        />
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default SushiContainer
+export default SushiContainer;
